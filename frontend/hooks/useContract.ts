0361b8ca08c2e876215c4512ab41c8c2fb90bb47
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-02-05 16:46:35 +0900

    型を修正

--- frontend/hooks/useContract.ts
+++ frontend/hooks/useContract.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
-import { InterfaceAbi, ethers } from "ethers";
+import { ethers } from "ethers";
 import AssetTokenizationArtifact from "../artifacts/AssetTokenization.json";
 import { AssetTokenization as AssetTokenizationType } from "../types";
 import { getEthereum } from "../utils/ethereum";
@@ -31,7 +31,7 @@ export const useContract = ({
     const getContract = useCallback(
         (
             contractAddress: string,
-            abi: InterfaceAbi,
+            abi: any,
             storeContract: (_: ethers.Contract) => void
         ) => {
             if (!ethereum) {
